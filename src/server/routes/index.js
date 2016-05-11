8e6e758d44f961e30edf1f333d054bd1ac004284
rlgaines <robertlg35@gmail.com>
2016-05-10 21:21:20 -0600

    commented out register route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -35,14 +35,15 @@ router.post('/register', function(req, res, next){
 		email:req.body.email,
 		password: req.body.password
 	})
- .then(function (member) {
- 	console.log(member)
+ // .then(function (member) {
+ // 	console.log(member)
       // var token = generateToken(member);
       // return Promise.resolve({
       //   token: token,
       //   data: member
       // });
-    })})
+    // })
+})
 
 
 //gets all decks 
